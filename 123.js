bc91ecc6ca8f7172aac59c31e4c4de32685755db
hogo18 <1452635819@qq.com>
2022-07-21 23:29:21 +0000

    删除 '123.js'

--- 123.js
+++
@@ -1,7 +0,0 @@
-{
-"spider":"https://agit.ai/1452635819/h/raw/branch/master/jar/qim.jar",//就留这一句
-//"sites":[
-//{"key":"Gitcafe","name":"小纸
-// 条","type":3,"api":"csp_GitCafe","searchable":1,"quickSearch":1,"filterable":1},
-//..........
-}
