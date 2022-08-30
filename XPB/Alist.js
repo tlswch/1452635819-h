536b840adf6c8dd243cff3257920edbd8b6dad05
hogo18 <1452635819@qq.com>
2022-08-30 02:33:16 +0000

    更新 'XPB/Alist.js'

--- XPB/Alist.js
+++ XPB/Alist.js
@@ -6,5 +6,9 @@
   "帅盘": "https://hi.shuaipeng.wang",
   "轻弹浅唱": "https://g.xiang.lol",
   "嗨翻": "https://pan.hikerfans.com",
-  "一只鱼": "https://alist.youte.ml"
+  "一只鱼": "https://alist.youte.ml",
+  "晴园的宝藏库": "https://alist.52qy.repl.co",
+  "听闻网盘": "https://wangpan.sangxuesheng.com",
+  "神族九帝": "https://alist.shenzjd.com",
+  "姬路白雪の资源小站": "https://pan.jlbx.xyz"
 }
