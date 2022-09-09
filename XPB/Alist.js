32649c22f64f10996c6265e258b647bc458d33ee
hogo18 <1452635819@qq.com>
2022-09-09 06:20:51 +0000

    上传文件至 'XPB'

--- XPB/Alist.js
+++ XPB/Alist.js
@@ -1,25 +1,15 @@
-{ 
-  "小兵网盘":"https://6vv.app",
-  "七米蓝": "https://al.chirmyram.com",
-  "9t网盘": "https://drive.9t.ee",
-  "一只鱼": "https://alist.youte.ml",
-  "听闻网盘": "https://wangpan.sangxuesheng.com",
-  //"星梦网盘": "https://pan.bashroot.top/",
-  //"A2影音库":"http://37.16.3.89/",
-  "米奇妙屋": "https://anime.mqmmw.ga",
+{
+  "米奇妙妙屋": "https://anime.mqmmw.ga",
   "小光盘": "https://alist.xiaoguanxiaocheng.life",
   "资源小站": "https://960303.xyz/",
   "菊花盘": "https://pan.142856.xyz",
   "帅盘": "https://hi.shuaipeng.wang",
-  "非盘":"http://www.feifwp.top",
   "轻弹浅唱": "https://g.xiang.lol",
-  "嗨翻网盘": "https://pan.hikerfans.com",
-  "晴园宝库": "https://alist.52qy.repl.co",
+  "嗨翻": "https://pan.hikerfans.com",
+  "一只鱼": "https://alist.youte.ml",
+  "晴园的宝藏库": "https://alist.52qy.repl.co",
+  "听闻网盘": "https://wangpan.sangxuesheng.com",
   "神族九帝": "https://alist.shenzjd.com",
-  //"小江网盘":"https://dyj.me/视频",
-  //"触光网盘":"https://pan.ichuguang.com/",
-  //"好汉盘吧":"https://8023.haohanba.cn/",
-  "姬路白雪": "https://pan.jlbx.xyz",
-  "梦拾网盘": "http://breadmyth.asuscomm.com:22222",
-  "DISK":"http://124.222.140.243:8080/"
+  "姬路白雪の资源小站": "https://pan.jlbx.xyz",
+  "七米蓝": "https://al.chirmyram.com"
 }
