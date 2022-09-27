5b86ecf1037c8549a6c4d5a5136e687c8cef60a5
hogo18 <1452635819@qq.com>
2022-09-27 10:10:40 +0000

    更新 'XPB/555dyPQ.js'

--- XPB/555dyPQ.js
+++ XPB/555dyPQ.js
@@ -6,6 +6,7 @@
   "线路标题":"<span>&&</span>",
   "播放数组":"class=\"module-play-list-content&&</div>",
   "分类":"电影$1#剧集$2#综艺$3#动漫$4#福利$124",
+  "类型":"clan://TVBox/筛选/555筛选.json",
   "分类url":"https://www.555yy1.com/vodshow/{cateId}-{area}-{by}-{class}-{lang}----{catePg}---{year}.html",
       "筛选": {
     "1": [
