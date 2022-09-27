7d0076fe43049362aa98fbb5e768c82af39e6266
hogo18 <1452635819@qq.com>
2022-09-27 07:09:49 +0000

    更新 'XPB/555dyPQ.js'

--- XPB/555dyPQ.js
+++ XPB/555dyPQ.js
@@ -2,7 +2,7 @@
   "免嗅":"1",
   "播放标题":"span>&&<",
   "副标题":"class=\"module-item-note\">&&</div>",
-  "线路数组":"class=\"module-tab-itemtab-item&&</div>",
+  "线路数组":"class=\"module-tab-item tab-item&&</div>",
   "线路标题":"<span>&&</span>",
   "播放数组":"class=\"module-play-list&&</div>",
   "分类":"电影$1#剧集$2#综艺$3#动漫$4#福利$124",
