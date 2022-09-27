08407a4e700a4ad97630cdb2edf153d6fa98ca64
hogo18 <1452635819@qq.com>
2022-09-27 08:29:06 +0000

    更新 'XPB/555dyPQ.js'

--- XPB/555dyPQ.js
+++ XPB/555dyPQ.js
@@ -4,7 +4,7 @@
   "副标题":"class=\"module-item-note\">&&</div>",
   "线路数组":"class=\"module-tab-item tab-item&&</div>",
   "线路标题":"<span>&&</span>",
-  "播放数组":"class=\"module-play-list&&</div>",
+  "播放数组":"class=\"module-play-list-content&&</div>",
   "分类":"电影$1#剧集$2#综艺$3#动漫$4#福利$124",
   "分类url":"https://www.555yy1.com/vodshow/{cateId}-{area}-{by}-{class}-{lang}----{catePg}---{year}.html",
       "筛选": {
