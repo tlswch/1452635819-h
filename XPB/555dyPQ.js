273b51be3443321384a5b8ed6d69c069842e0ee8
hogo18 <1452635819@qq.com>
2022-10-04 02:06:42 +0000

    更新 'XPB/555dyPQ.js'

--- XPB/555dyPQ.js
+++ XPB/555dyPQ.js
@@ -1,13 +1,14 @@
-{
-  "免嗅":"1",
-  "播放标题":"span>&&<",
-  "副标题":"class=\"module-item-note\">&&</div>",
-  //"线路数组":"class=\"module-tab-item tab-item&&</div>",
-  //"线路标题":"<span>&&</span>",
-  "播放数组":"class=\"module-play-list-content&&</div>",
-  "分类":"电影$1#剧集$2#综艺$3#动漫$4#福利$124",
-  //"类型":"clan://TVBox/筛选/555筛选.json",
-  "分类url":"https://www.555yy1.com/vodshow/{cateId}-{area}-{by}-{class}-{lang}----{catePg}---{year}.html",
+{"免嗅":"1",
+"播放标题":"span>&&<",
+"导演": "导演：&&</div",
+"主演": "主演：&&</div",
+"分类":"数字||福利$124",
+"副标题":"class=\"module-item-note\">&&</div>",
+"线路数组": "class=\"module-tab-item tab-item&&</div>",
+"线路标题": "<span>&&</span>",
+"类型":"0",
+"播放数组":"class=\"module-play-list-content&&</div>",
+"分类url":"https://www.555yy1.com/vodshow/{cateId}-{area}-{by}-{class}-{lang}----{catePg}---{year}.html",
       "筛选": {
     "1": [
                         {
