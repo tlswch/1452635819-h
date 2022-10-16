8722d23fdcab03a3c579e6e57e98322c4a24af7b
hogo18 <1452635819@qq.com>
2022-10-16 11:58:39 +0000

    更新 'Py/czsy.py'

--- Py/czsy.py
+++ Py/czsy.py
@@ -59,7 +59,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 
     def categoryContent(self, tid, pg, filter, extend):
         result = {}
-        url = 'https://www.czspp.com/{0}/page/{1}'.format(tid, pg)
+        url = 'https://www.czspp.com/?btwaf=86276706{0}/page/{1}'.format(tid, pg)
         rsp = self.fetch(url)
         root = self.html(self.cleanText(rsp.text))
         aList = root.xpath("//div[contains(@class,'mi_cont')]//ul/li")
